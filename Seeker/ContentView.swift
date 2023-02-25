119a5895a02d0ee9f2b150bc5f23384a988c5829
timli1020 <zhaotian.li@uconn.edu>
2023-02-25 12:59:46 -0500

    Initial Commit

---
+++ Seeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Seeker
+//
+//  Created by Tim Li on 2/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Seeker/SeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeekerApp.swift
+//  Seeker
+//
+//  Created by Tim Li on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
